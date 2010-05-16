1a268195c82f48e1b9303b9b76aa42191696f3bc
Stevie Frederick <leflor@gmail.com>
2010-05-16 16:25:32 -0400

    This is functional based on project desicription

--- src/linux/charizer/charizerF.asm
+++ src/linux/charizer/charizerF.asm
@@ -1,24 +1,35 @@
 SECTION .DATA
 	bigF:     db 'FFFFFFF',10
 	bigFlen:  equ $-bigF
-	lilF:	  db 'FF',2
-	lilFlen:     equ $-lilF
+	lilF:	  db 'F',10
+	lilFlen:  equ $-lilF
 SECTION .TEXT
 	GLOBAL _start
 
 _start:
 
+	mov eax,4            
+	mov ebx,1            
+	mov ecx,bigF
+	mov edx,bigFlen
+	int 80h
+	mov eax,4            
+	mov ebx,1            
+	mov ecx,lilF	
+	mov edx,lilFlen	
+	int 80h
+	mov eax,4            
+	mov ebx,1            
+	mov ecx,lilF	
+	mov edx,lilFlen	
+	int 80h
+	mov eax,4            
+	mov ebx,1            
+	mov ecx,bigF
+	mov edx,bigFlen
+	int 80h
 
-
-	; Write 'Hello world!' to the screen
-	mov eax,4            ; 'write' system call
-	mov ebx,1            ; file descriptor 1 = screeni
-	mov ecx,bigF       ; string to write
-	mov edx,bigFlen            ; length of string to write
-	int 80h              ; call the kernel
-
-	; Terminate program
-	mov eax,1            ; 'exit' system call
-	mov ebx,0            ; exit with error code 0
-	int 80h              ; call the kernel
+	mov eax,1           
+	mov ebx,0          
+	int 80h          
 
